0d31525b03682a78a87ca1349dfdcb60b8d758ef
axiner <axiner@Kexins-iMac.local>
2016-03-27 02:07:35 -0400

    Getting there

--- Mars_Rover.rb
+++ Mars_Rover.rb
@@ -1,46 +1,64 @@
 class Rover
   attr_accessor :x, :y, :direction
     def initialize(x,y,direction)
-      @@x = x
-      @@y = y
-      @@direction = direction
+      @x = x
+      @y = y
+      @direction = direction
     end
 
     def read_instruction(instructions)
-      puts "What's your instructions?"
-        instructions = gets
-          instructions.split(",")
-            if instructions.include?("L"||"R")
-                instructions.turn("left_right")
+      puts "What are your instructions?"
+        instructions = gets.split(//)
+            if instructions == "L"
+              self.turn("left_right")
+            elsif instructions == "R"
+                self.turn("left_right")
                 else
-                  instructions.move
-            end
+                  self.move
+         end
     end
 
       def turn(left_right)
-        puts "Which direction is the rover moving?"
-         left_right = gets
-
-         if direction = "N" && left_right = "L"
-           "S"
-         elsif direction = "N" && left_right = "R"
-           "E"
-         elsif direction = "E" && left_right = "L"
-           "N"
-         elsif direction = "E" && left_right = "R"
-           "S"
-         elsif direction = "S" && left_right= "L"
-           "E"
-         elsif direction = "S" && left_right = "R"
-           "W"
-         elsif direction = "W" && left_right = "L"
-           "S"
-         else direction = "W" && left_right = "R"
-           "N"
-           end
+         if @direction == "N" && left_right == "L"
+           @direction = "S"
+         elsif @direction == "N" && left_right == "R"
+           @direction = "E"
+         elsif @direction == "E" && left_right == "L"
+           @direction = "N"
+         elsif @direction == "E" && left_right == "R"
+           @direction = "S"
+         elsif @direction == "S" && left_right== "L"
+           @direction = "E"
+         elsif @direction = "S" && left_right == "R"
+           @direction = "W"
+         elsif @direction = "W" && left_right == "L"
+           @direction = "S"
+         else @direction = "W" && left_right == "R"
+           @directiob = "N"
       end
+    end
 
       def move
+      if @direction == "N" then @y = y + 1
+        elsif @direction == "E" then @x = x + 1
+          elsif @direction == "S" then @y = y - 1
+      elsif @direction == "W" then @x = x - 1
+      else
+        puts "Eh?"
+      end
+      end
 
-      end 
+
+      def final_position
+        puts "#{@x} #{@y} #{@direction} "
+      end
+
 end
+
+rover1 = Rover.new(1,2,'N')
+rover1.read_instruction("instructions")
+rover1.final_position
+
+rover2 = Rover.new(3,3,"E")
+rover2.read_instruction("instructions")
+rover2.final_position
